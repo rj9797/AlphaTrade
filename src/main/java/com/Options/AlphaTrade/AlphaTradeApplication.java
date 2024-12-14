d9e86005f7b6fe34d75158570eb4f9782e67e7f3
Rushabh Joshi <rushabh.joshi97@gmail.com>
2024-12-14 16:10:46 +0530

    project initialized

---
+++ src/main/java/com/Options/AlphaTrade/AlphaTradeApplication.java
@@ -0,0 +1,13 @@
+package com.Options.AlphaTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Options/AlphaTrade/AlphaTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Options.AlphaTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
